5846ec90132241277f233d3702b6046eeaba8307
smsm85200 <147330112+smsm85200@users.noreply.github.com>
2023-12-23 01:42:03 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    API_ID = 7715737 
+    API_HASH = "241e23bd490bfad03f0da62d4f5d8a20" 
+    TOKEN = "6095625065:AAHobldI5Js4Cf3Ws9zhvgCA9NmieRYlpIM" 
+    START_PIC = "https://telegra.ph/file/f146618e52922ba911f03.jpg" #هنا حط رابط صورة تلكراف
+    CHAT = "-1001849313628" 
+
+
+
