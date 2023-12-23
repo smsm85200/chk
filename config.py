bfaaaac485b5236e55961bf77b62f400ea25cc6d
smsm85200 <147330112+smsm85200@users.noreply.github.com>
2023-12-23 16:36:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config:
     API_ID = 7715737 
     API_HASH = "241e23bd490bfad03f0da62d4f5d8a20" 
-    TOKEN = "6095625065:AAHobldI5Js4Cf3Ws9zhvgCA9NmieRYlpIM" 
+    TOKEN = "6950954118:AAF4LyHE9XnVuJYxwRQoWzIMvT11iyInQYs" 
     START_PIC = "https://telegra.ph/file/f146618e52922ba911f03.jpg" #هنا حط رابط صورة تلكراف
     CHAT = "-1001849313628" 
 
